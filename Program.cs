00f027ad8fea462fee4053c0a811c0a93582aea9
Praveen <rompicharalaPraveen@gmail.com>
2021-09-23 18:17:29 +0530

    Dictionary

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDictionary<int, string> num = new Dictionary<int, string>();
+            num.Add(1, "one");
+            num.Add(3, "Three");
+            num.Add(2, "Two");
+            foreach (KeyValuePair<int, string> kvp in num)
+                Console.WriteLine("key:{0},Value:{1}",kvp.Key,kvp.Value);
+        }
+    }
+}
